4803f2e6b61d593ca3689ffe081f62aa9fa4c0b7
dkhole <dkhlee419@gmail.com>
2021-03-21 20:59:30 +1100

    testing

--- blog/yeh-is-lit.md
+++ blog/yeh-is-lit.md
@@ -1,5 +1,5 @@
 ---
-path: Hello
+slug: Hello
 date: 2021-03-21T09:05:24.940Z
 title: YEH IS LIT
 ---
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,27 @@
 import React from "react"
+import { graphql } from "gatsby";
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Home({ data }) {
+// data.markdownRemark holds your data
+  const { markdownRemark } = data;
+  const { frontmatter } = markdownRemark;
+
+  return (
+    <div className="blog-post-container">
+      {
+        frontmatter.slug
+      }
+    </div>
+  );
 }
+
+export const pageQuery = graphql`
+  query MyQuery {
+    markdownRemark {
+      frontmatter {
+        title
+        slug
+      }
+    }
+  }
+`

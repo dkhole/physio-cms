e69f8593d58dead90b0888255ea377cee93a14a2
dkhole <dkhlee419@gmail.com>
2021-03-21 21:34:27 +1100

    title field should be working with netlify cms

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,30 +2,30 @@ import React from "react"
 import { graphql } from "gatsby";
 
 export default function Home({ data }) {
-// data.markdownRemark holds your data
-//  const { allMarkdownRemark } = data;
-//  const mdFiles = allMarkdownRemark.edges;
+  const { allMarkdownRemark } = data;
+  const mdFiles = allMarkdownRemark.edges;
 
-  //console.log(mdFiles);
+  console.log(mdFiles);
+
   return (
     <div className="blog-post-container">
-      hello
+      {
+        mdFiles[0].node.frontmatter.title
+      }
     </div>
   );
 }
 
-/**export const pageQuery = graphql`
+export const pageQuery = graphql`
   query MyQuery {
     allMarkdownRemark {
-      edges{
+      edges {
         node {
           frontmatter {
-            date
-            slug
             title
           }
         }
       }
     }
   }
-`**/
+`
